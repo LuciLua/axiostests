d80ed4be94f75b049fa2c063ec81daf1c92813c8
LuciLua <luci.lua81@gmail.com>
2021-11-20 15:46:46 -0300

    🔆 Axios | Get | Post

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-const url = 'estados.json'
-
-const newEstado = {
-    nome: "Minal",
-    sigla: "ML"
-}
-
+const url = 'http://localhost:8080/api'
 function getUser(){
-    
-    // axios(url)
-    // .then(resp=>{
-    //     const itens = resp.data.reduce( // ja tenho json convertido em resp.data
-    //         (html, estado) => html + `<li>${estado.nome}</li>`, '' 
-    //         )
-    //         document.body.insertAdjacentHTML("beforeend", itens)
-    // })
-    
+        
     axios.get(url)
     .then(resp => {
         const estados = Array.from(resp.data)
@@ -22,30 +8,43 @@ function getUser(){
             const tr = document.createElement('tr')
             const tdNome = document.createElement('td')
             const tdSigla = document.createElement('td')
+            const tdId = document.createElement('td')
             const table = document.querySelector('table > tbody')
 
+            tdId.textContent = e.id
             tdNome.textContent = e.nome
             tdSigla.textContent = e.sigla
 
             tr.appendChild(tdNome)
             tr.appendChild(tdSigla)
+            tr.appendChild(tdId)
             table.appendChild(tr)
         })
     })
     .catch(error => console.log(error))
 }
-
 getUser()
 
+document.querySelector('button').onclick = e => {
 
+var sigla = document.getElementById('sigla').value
+var nome = document.getElementById('nome').value
+
+let newEstado = {
+    nome: nome,
+    sigla: sigla
+}
 
 function addNewEstado(){
     axios.post(url, newEstado)
     .then(resp => {
-        console.log('resp')
+        console.log(resp.data)
+        return resp
     })
     .catch(error => console.log('erro:'+ error))
+    }
+    
+   addNewEstado()   
 }
 
-addNewEstado()
 
--- server.js
+++ server.js
@@ -1,7 +1,92 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
+app.use(cors())
 
 // middleware
 app.use(express.static('.')) // provê arquivos estaticos a partir da aplicaocao desse middleaware
+
+app.use(express.json())
+
+  let estados = [
+    {
+        id: 1,
+        nome: 'luci',
+        sigla: 'LU'
+    }]
+
+  
+  app.route('/api').get((req, res) => { res.json(estados) })
+
+  
+  app.route('/api/:id').get((req, res) => {
+    const userId = req.params.id
+  
+    const user = estados.find(user => Number(user.id) === Number(userId))
+  
+    if (!user) {
+      return res.json('User nor found!')
+    }
+  
+    res.json(user)
+  })
+  
+  //inserir
+  app.route('/api').post((req, res) => {
+    const lastId = estados[estados.length - 1].id
+
+    estados.push({
+      id: lastId + 1,
+      nome: req.body.nome,
+      sigla: req.body.sigla,
+    })
+
+    res.json('Saved user')
+  })
+  
+
+
+  //update
+//   app.route('/api/:id').put((req, res) => {
+//     const userId = req.params.id
+  
+//     const user = estados.find(user => Number(user.id) === Number(userId))
+  
+//     if (!user) {
+//       return res.json('User nor found!')
+//     }
+  
+//     const updatedUser = {
+//       ...user,
+//       name: req.body.nome,
+//       avatar: req.body.sigla,
+//     }
+  
+//     estados = estados.map(user => {
+//       if (Number(user.id) === Number(userId)) {
+//         user = updatedUser
+//       }
+//       return user
+//     })
+  
+//     res.json("Updated user")
+//   })
+
+  
+
+  //delete
+//   app.route('/api/:id').delete((req, res) => {
+//     const userId = req.params.id
+  
+//     estados = estados.filter(user => Number(user.id) !== Number(userId))
+  
+//     res.json('Deleted User')
+//   })
+
+
+
+
 // ouvindo porta 8080
-app.listen(8080, () => console.log('Executando...'))
+app.listen(8080, () => console.log('Executando... Ouvindo porta 8080'))
